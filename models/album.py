bd26371c8cd4cf8ed93f4b84157278b670457bb5
Gary Darnell <gdarnell9@gmail.com>
2021-06-01 15:13:59 +0100

    console finished

---
+++ models/album.py
@@ -0,0 +1,9 @@
+from models.artist import Artist
+
+
+class Album:
+    def __init__(self, album_name, artist, genre, id=None):
+        self.album_name = album_name
+        self.artist = artist
+        self.genre = genre
+        self.id = id
